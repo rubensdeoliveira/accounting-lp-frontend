c07e3bbbfd595ccc16cf91d62da6ec1c77da567a
Rubens de Oliveira <rubensojunior6@gmail.com>
2023-01-26 23:51:07 -0300

    feat: adjusting colors

--- src/app/_presentation/components/learn-more-button/learn-more-button.component.tsx
+++ src/app/_presentation/components/learn-more-button/learn-more-button.component.tsx
@@ -10,7 +10,7 @@ export function LearnMoreButton({
 }: LearnMoreButtonModel) {
   return (
     <button {...rest} className="flex">
-      <span className="flex items-center gap-[0.625rem] text-sm font-medium leading-[1.3] text-red500 dark:text-red500 md:text-[0.9375rem] lg:text-base">
+      <span className="flex items-center gap-[0.625rem] text-sm font-medium leading-[1.3] text-[#F50D32] dark:text-red500 md:text-[0.9375rem] lg:text-base">
         {label}
         <FiArrowRight size={24} />
       </span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       colors: {
         'gray300': '#D9D9D9',
         'gray500': '#2A2A39',
-        'gray900': '#232020',
+        'gray900': '#2E2E30',
         'red500': '#d2140a',
         'red900': '#6C0C0F'
 
