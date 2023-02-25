354a3214ec9edc808debe7caba853fae480acb8f
Rubens Junior <rubensojunior6@gmail.com>
2023-02-25 14:09:55 -0300

    chore: change eslint config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,9 @@
+import '@rubensdeoliveira-ui/react/dist/index.css'
+
 import { AppProps, type AppType } from 'next/app'
 import { type Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
-import Script from 'next/script'
 
-import '@/client/application/styles/global.css'
-import '@rubensdeoliveira-ui/react/dist/index.css'
 import { Providers } from '@/client/application/providers'
 import { api } from '@/shared/utils'
 
@@ -15,10 +14,6 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <Providers>
-        <Script
-          src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"
-          strategy="beforeInteractive"
-        />
         <Component {...pageProps} />
       </Providers>
     </SessionProvider>
